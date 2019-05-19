bef3530eab2e1c763bfbaf4924e8bc452821b994
johnware15 <contact@johndware.com>
2019-05-19 15:07:37 -0700

    Import test database from jsonplaceholder.typecode.com.

--- test/src/App.vue
+++ test/src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'app',
@@ -30,11 +31,20 @@ export default {
       this.todos = this.todos.filter(todo => todo.id !== id);
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo]
+      const { title, completed } = newTodo
+
+      axios.post('http://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err))
     }
   },
   created() {
-    
+    axios.get('http://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err))
   }
 }
 </script>
